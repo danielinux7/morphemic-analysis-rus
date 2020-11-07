646478073b918f3c9729340b25433fb1403b27bb
Nart Tlisha <daniel.abzakh@gmail.com>
2020-11-07 23:20:09 +0300

    Update rules_corrected+maxmatch.py

--- rules_corrected+maxmatch.py
+++ rules_corrected+maxmatch.py
@@ -1049,34 +1049,3 @@ def analysis():
                 word_parts.insert(i, new_lst[a])
                 i += 1
         return word_parts
-
-while True:
-    #word_or_text = input('Выберите режим работы: слово (с) или текст (т) (для выхода наберите "exit"): ')
-    word_or_text = 'т'
-    if word_or_text == 'exit':
-        break
-    if word_or_text == 'с':
-        word = input('Введите слово: ')
-        print(analysis())
-    elif word_or_text == 'т':
-        #file_name = input('Укажите название файла: ')
-        file_name = 'ru.txt'
-        morph = pymorphy2.MorphAnalyzer()
-        with open(file_name, encoding='utf-8') as f:
-            text = f.read().lower()
-            text_tokenized = nltk.word_tokenize(text)
-            new_text, information = '', []
-            for t in text_tokenized:
-                p_t = morph.parse(t)[0]
-                if t.isalpha() and p_t.tag.POS not in {'PREP', 'CONJ', 'PRCL', "INTJ"}:
-                    word = t
-                    word_print = word
-                    word_parts = analysis()
-                    new_text += ' | '.join([word_print, ' '.join(word_parts)]) + '\n'
-                    print(new_text)
-        #annotation_file = input('Введите название файла, в который Вам нужно сохранить результаты аннотирования: ')
-        annotation_file = 'annotation.txt'
-        with open(annotation_file, 'w', encoding='utf-8') as f:
-            f.write(new_text[:-1])
-            print('В файл {} были записаны результаты аннотирования.'.format(annotation_file))
-        break
